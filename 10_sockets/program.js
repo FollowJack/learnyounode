15fbae188094558f48d85f54f07ad08da5a8e426
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-05-18 09:42:20 +0200

    Finished chapter 11 File Server

--- 10_sockets/program.js
+++ 10_sockets/program.js
@@ -3,12 +3,17 @@ const strftime = require('strftime')
 
 const portNumber = process.argv[2];
 
+// TCP Server Transmission Control Protocol/Internet Protocol 
+
+
 // result
 // "YYYY-MM-DD hh:mm"
 const server = net.createServer( (socket) => {
-    var data = strftime('%Y-%m-%d %R')+'\n'
+    var data = strftime('%Y-%m-%d %R'+'\n')
     socket.write(data)
     socket.end()
 })
 
 server.listen(portNumber)
+
+
